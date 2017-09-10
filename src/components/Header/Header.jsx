e6774e78e48d5d27290c1ea6eb7602531f29f44f
Jack <j.evans.1310@gmail.com>
2017-09-10 15:45:08 +0100

    create animated sideBarMenu

--- src/components/Header/Header.jsx
+++ src/components/Header/Header.jsx
@@ -4,42 +4,18 @@ import './Header.scss'
 
 class Header extends Component {
 
-  constructor (props) {
-    super(props)
-    this.state = { isExpanded: false}
-  }
-
-  handleMenuClickEvent () {
-    this.setState({
-      isExpanded: !this.state.isExpanded
-    })
-  }
-
   render () {
-    let menu
-
-    if (this.state.isExpanded) {
-      menu = (
-        <div className='Header__left-nav'>
-          I'm a menu
-        </div>
-      )
-    }
-
     return (
-      <div className='Header'>
-        <div className='Header__top-nav'>
-          <div className='Header__left-container'>
-            <div className="Header__menu">
-              <button type='button' className='Header__menu-button' onClick={() => this.handleMenuClickEvent()}>
-                <Icon className='Header__menu-icon' name='icon--menu' />
-              </button>
-            </div>
-            {/*<a className='Header__title' href='/'>Welcome to React</a>*/}
+      <header className='Header'>
+        <div className='Header__left-container'>
+          <div className="Header__menu">
+            <button type='button' className='Header__menu-button' onClick={this.props.onClick}>
+              <Icon className='Header__menu-icon' name='icon--menu' />
+            </button>
           </div>
+          {/*<a className='Header__title' href='/'>Welcome to React</a>*/}
         </div>
-        {menu}
-      </div>
+      </header>
     )
   }
 }
