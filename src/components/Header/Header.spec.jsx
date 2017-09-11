3e17488f37b612946efd4b33a9fb5fb479a597ca
Jack <j.evans.1310@gmail.com>
2017-09-11 13:05:54 +0100

    add test for callback function

--- src/components/Header/Header.spec.jsx
+++ src/components/Header/Header.spec.jsx
@@ -21,4 +21,15 @@ describe('Header..jsx', () => {
     )
     expect(shallow(<Header onClick={cbFunction} />).contains(expectedRender)).toBe(true)
   })
+
+  describe('when the button is clicked', () => {
+    it('calls the callback function', () => {
+      const cbFunction = () => {
+        expect(1).toEqual(1)
+      }
+
+      const wrapper = shallow(<Header onClick={cbFunction} />)
+      wrapper.find('button').simulate('click')
+    })
+  })
 })
