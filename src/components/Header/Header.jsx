4b188f8f3fbbc3ef12ea34e43c8d51d5667d1ef5
Jack <j.evans.1310@gmail.com>
2017-09-13 21:32:05 +0100

    animate menu icon when pressed

--- src/components/Header/Header.jsx
+++ src/components/Header/Header.jsx
@@ -3,16 +3,15 @@ import './Header.scss'
 
 class Header extends Component {
   render () {
+    const buttonClassName = this.props.isOpen ? 'Header__left-nav-toggle Header__left-nav-toggle--active' : 'Header__left-nav-toggle'
     return (
       <header className='Header'>
-        <div className='Header__left-container'>
-          <div className='Header__left-nav-toggle-container'>
-            <button type='button' className='Header__left-nav-toggle' onClick={this.props.onClick}>
-              <span />
-            </button>
-          </div>
-          <a className='Header__title' href='/'>Welcome to React</a>
+        <div className='Header__left-nav-toggle-container'>
+          <button type='button' className={buttonClassName} onClick={this.props.onClick}>
+            <span className='Header__left-nav-icon' />
+          </button>
         </div>
+        <a className='Header__title' href='/'>Welcome to React</a>
       </header>
     )
   }
