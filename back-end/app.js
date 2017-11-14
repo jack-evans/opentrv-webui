20ceeb9e8f43f04ac34bebde0669c5e80225d117
Jack <j.evans.1310@gmail.com>
2017-11-14 23:47:46 +0000

    begin building the interaction with the device database

--- back-end/app.js
+++ back-end/app.js
@@ -4,7 +4,14 @@ const bodyParser = require('body-parser')
 const express = require('express')
 const router = require('./router.js')
 
+const DeviceDatabase = require('./lib/deviceDiscoveryService/deviceDatabase')
+const deviceDb = new DeviceDatabase()
+
 const app = express()
+
+deviceDb.initialise()
+
+app.set('deviceDb', deviceDb)
 
 app.use(bodyParser.json())
 
---
+++ back-end/lib/deviceDiscoveryService/deviceDatabase.js
@@ -0,0 +1,88 @@
+'use strict'
+
+// database to store basic information of devices
+
+const bunyan = require('bunyan')
+const cloudantRequestHelper = require('../utilities/cloudantRequestHelper')
+const databaseName = 'devices'
+const Promise = require('bluebird')
+//const uuidv4 = require('uuid/v4')
+
+const logger = bunyan.createLogger({name: 'device-discovery-service-db', serializers: bunyan.stdSerializers})
+
+function DeviceDatabase () {
+  this.cloudantInstance = cloudantRequestHelper.createCloudantConnection()
+  this.database = undefined
+  this.initPromise = undefined
+}
+
+DeviceDatabase.prototype.initialise = function () {
+  let self = this
+
+  self.initPromise = cloudantRequestHelper.createDatabase(self.cloudantInstance, databaseName)
+    .then(function () {
+      self.database = cloudantRequestHelper.useDatabase(self.cloudantInstance, databaseName)
+      return Promise.resolve()
+    })
+    .catch(function (error) {
+      logger.error(`Encountered error when attempting to initialise the '${databaseName}' database, reason: `, error)
+      return Promise.reject(error)
+    })
+}
+
+/*
+DeviceDatabase.prototype.createDevice = function (deviceDocument) {
+  let self = this
+
+  self.initPromise
+    .then(function () {
+      let id = uuidv4()
+
+      deviceDocument.id = id
+      deviceDocument._id = id
+
+      return cloudantRequestHelper.createDocument(self.database, databaseName, deviceDocument)
+    })
+    .catch(function (error) {
+      logger.error(`Encountered error when creating document in the '${databaseName}' database, reason: `, error)
+      return Promise.reject(error)
+    })
+}
+
+DeviceDatabase.prototype.getDeviceInformation = function (deviceDocumentId) {
+  let self = this
+
+  self.initPromise
+    .then(function () {
+      return cloudantRequestHelper.retrieveDocument(self.database, databaseName, deviceDocumentId)
+    })
+    .catch(function (error) {
+      logger.error(`Encountered error when retrieving document in the '${databaseName}' database, reason: `, error)
+      return Promise.reject(error)
+    })
+}
+
+DeviceDatabase.prototype.getAllDevices = function () {
+  let self = this
+
+  self.initPromise
+    .then(function () {
+      return cloudantRequestHelper.retrieveAllDocuments(self.database, databaseName)
+    })
+    .catch(function (error) {
+      logger.error(`Encountered error when retrieving documents in the '${databaseName}' database, reason: `, error)
+      return Promise.reject(error)
+    })
+}
+
+DeviceDatabase.prototype.updateDevice = function (deviceDocument) {
+  let self = this
+
+  self.initPromise
+    .then(function () {
+      return cloudantRequestHelper.updateDocument(self.database, databaseName, deviceDocument)
+    })
+}
+*/
+
+module.exports = DeviceDatabase
