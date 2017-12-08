765c08ad615c42bc1e5ca9c1dbc2edff76336dd4
Jack Evans <j.evans.1310@gmail.com>
2017-12-08 23:41:03 +0000

    add router paths to device discovery service

--- back-end/router.js
+++ back-end/router.js
@@ -1,7 +1,12 @@
 'use strict'
 
+const deviceDiscoveryService = require('./lib/deviceDiscoveryService/deviceDiscovery')
 const router = require('express').Router()
 const packageJson = require('../package.json')
+
+// DeviceDiscoveryService
+router.post('/devices', deviceDiscoveryService.createDeviceRequestHandler)
+router.get('/devices', deviceDiscoveryService.discoverAllDevicesRequestHandler)
 
 router.get('/test', (req, res) => {
   res.status(200).send({'time': new Date(), 'name': packageJson.name, 'version': packageJson.version})
