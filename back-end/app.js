e8a9645690368818aa0a1e1474172c9a057e6146
Jack Evans <j.evans.1310@gmail.com>
2017-12-08 23:39:23 +0000

    fix to make request object contain the devicedb

--- back-end/app.js
+++ back-end/app.js
@@ -2,19 +2,21 @@
 
 const bodyParser = require('body-parser')
 const express = require('express')
-const router = require('./router.js')
-
-const DeviceDatabase = require('./lib/deviceDiscoveryService/deviceDatabase')
-const deviceDb = new DeviceDatabase()
 
 const app = express()
 
+const DeviceDatabase = require('./lib/deviceDiscoveryService/deviceDatabase')
+const deviceDb = new DeviceDatabase()
 deviceDb.initialise()
 
-app.set('deviceDb', deviceDb)
+app.use((req, res, next) => {
+  req.deviceDb = deviceDb
+  next()
+})
 
 app.use(bodyParser.json())
 
+const router = require('./router.js')
 app.use('/api/v1', router)
 
 module.exports = app
