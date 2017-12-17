b22909a31774acd2b6864434f0333be9a5c61034
Jack Evans <j.evans.1310@gmail.com>
2017-12-17 22:56:04 +0000

    button appears in the middle of the screen when the user has no devices

--- src/components/Overview/Overview.jsx
+++ src/components/Overview/Overview.jsx
@@ -1,7 +1,20 @@
 import React, { Component } from 'react'
 import OverviewHeader from '../OverviewHeader/OverviewHeader'
+import { Button, Search } from 'carbon-components-react'
 
 class Overview extends Component {
+  constructor (props) {
+    super(props)
+    this.search = ''
+    this.state = {
+      visibleDevices: Overview.retrieveDevices()
+    }
+  }
+
+  static retrieveDevices () {
+    return []
+  }
+
   /**
    * render method
    *
@@ -9,9 +22,38 @@ class Overview extends Component {
    * @returns {HTML} - Overview component
    */
   render () {
+    let overviewContent
+
+    if (this.state.visibleDevices.length < 1) {
+      overviewContent = (
+        <Button
+          className='Overview__discover-button'
+          icon='add--outline'
+          iconDescription='Add devices'
+          onClick={() => console.log('button clicked')}
+        >
+          Discover my devices
+        </Button>
+      )
+    } else {
+      // This is where the tiles for each device are build up
+    }
+
     return (
       <div className='Overview'>
         <OverviewHeader />
+        <div className='Overview__search-bar-container'>
+          <Search
+            className='Overview__search-bar'
+            small
+            id='device-search'
+            labelText='Search'
+            placeHolderText='Search Devices'
+          />
+        </div>
+        <div className='Overview__content'>
+          {overviewContent}
+        </div>
       </div>
     )
   }
