beae3672f1b0a03b0aa39741c59b7a20f2374ee6
Jack Evans <j.evans.1310@gmail.com>
2018-02-18 17:05:59 +0000

    change to function component

--- src/components/Header/Header.jsx
+++ src/components/Header/Header.jsx
@@ -1,27 +1,19 @@
-import React, { Component } from 'react'
+import React from 'react'
 
-class Header extends Component {
-  /**
-   * render method
-   *
-   * renders the header component of the UI experience
-   * @returns {HTML} - Header component
-   */
-  render () {
-    const buttonClassName = this.props.isOpen ? 'Header__left-nav-toggle Header__left-nav-toggle--active' : 'Header__left-nav-toggle'
-    return (
-      <header className='Header'>
-        <div className='Header__left-nav-toggle-container'>
-          <button type='button' className={buttonClassName} onClick={this.props.onClick}>
-            <span className='Header__left-nav-icon' />
-          </button>
-        </div>
-        <a className='Header__title' href='/'>
-          <h3>OpenTRV</h3>
-        </a>
-      </header>
-    )
-  }
+const Header = ({isOpen, onClick}) => {
+  const buttonClassName = isOpen ? 'Header__left-nav-toggle Header__left-nav-toggle--active' : 'Header__left-nav-toggle'
+  return (
+    <header className='Header'>
+      <div className='Header__left-nav-toggle-container'>
+        <button type='button' className={buttonClassName} onClick={onClick}>
+          <span className='Header__left-nav-icon' />
+        </button>
+      </div>
+      <a className='Header__title' href='/'>
+        <h3>OpenTRV</h3>
+      </a>
+    </header>
+  )
 }
 
 export default Header
