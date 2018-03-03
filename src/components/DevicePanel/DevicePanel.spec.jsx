d637df26627c83529de3040d4027c76d3f8fdd9b
Jack Evans <j.evans.1310@gmail.com>
2018-03-03 14:04:26 +0000

    add more unit tests

--- src/components/DevicePanel/DevicePanel.spec.jsx
+++ src/components/DevicePanel/DevicePanel.spec.jsx
@@ -6,13 +6,28 @@ import Adapter from 'enzyme-adapter-react-16'
 Enzyme.configure({ adapter: new Adapter() })
 
 describe('DevicePanel.jsx', () => {
-  it('renders without crashing', () => {
-    const match = {
-      params: {
-        id: '1234'
-      }
+  const match = {
+    params: {
+      id: '1234'
     }
+  }
 
+  it('renders without crashing', () => {
     expect(shallow(<DevicePanel match={match} />)).toMatchSnapshot()
+  })
+
+  describe('when the isLoading state is true', () => {
+    it('returns null', () => {
+      const wrapper = shallow(<DevicePanel match={match} />)
+      expect(wrapper.type()).toBe(null)
+    })
+  })
+
+  describe('when the isLoading state is false', () => {
+    it('returns the component', () => {
+      const wrapper = shallow(<DevicePanel match={match} />)
+      wrapper.setState({isLoading: false})
+      expect(wrapper.find('.DevicePanel')).toHaveLength(1)
+    })
   })
 })
