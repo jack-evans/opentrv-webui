4b3c4b0bdcbfa4f2add72b67a2bb1ecf7ce7de09
Jack Evans <j.evans.1310@gmail.com>
2018-03-17 15:52:59 +0000

    change to let

--- src/utils/makeRequest.js
+++ src/utils/makeRequest.js
@@ -9,7 +9,7 @@ export function checkStatus (response) {
   if (response.status >= 200 && response.status < 300) {
     return response
   } else {
-    var error = new Error(response.statusText)
+    let error = new Error(response.statusText)
     error.response = response
     throw error
   }
