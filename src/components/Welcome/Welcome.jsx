9ac94ffbefa6de78bf5151e67c32d05e3ff2246f
Jack Evans <j.evans.1310@gmail.com>
2018-03-21 12:26:09 +0000

    add basic buttons for register and login

--- src/components/Welcome/Welcome.jsx
+++ src/components/Welcome/Welcome.jsx
@@ -1,10 +1,26 @@
 import React, { Component } from 'react'
+import { Button } from 'carbon-components-react'
 
 class Welcome extends Component {
   render () {
     return (
-      <div>
+      <div className='Welcome'>
         <img src={require('./opentrv.jpg')} alt='OpenTRV logo' />
+        <div className='Welcome__buttons'>
+          <Button
+            kind='secondary'
+            className='Welcome__register-button'
+            onClick={() => (console.log('registered'))}
+          >
+            Register
+          </Button>
+          <Button
+            className='Welcome__login-button'
+            onClick={() => (console.log('logged in'))}
+          >
+            Login
+          </Button>
+        </div>
       </div>
     )
   }
--- src/components/Welcome/Welcome.spec.jsx
+++ src/components/Welcome/Welcome.spec.jsx
@@ -1,6 +1,6 @@
 import React from 'react'
 import Welcome from './Welcome'
-import Enzyme, { shallow } from 'enzyme'
+import Enzyme, { shallow, mount } from 'enzyme'
 import Adapter from 'enzyme-adapter-react-16'
 
 Enzyme.configure({ adapter: new Adapter() })
@@ -8,5 +8,53 @@ Enzyme.configure({ adapter: new Adapter() })
 describe('Welcome.jsx', () => {
   it('renders without crashing', () => {
     expect(shallow(<Welcome />)).toMatchSnapshot()
+  })
+
+  describe('when the register button is pressed', () => {
+    let wrapper
+    let consoleLogSpy
+
+    beforeEach(() => {
+      consoleLogSpy = jest.spyOn(console, 'log')
+      wrapper = mount(<Welcome />)
+      wrapper.find('.Welcome__register-button').at(1).simulate('click')
+    })
+
+    afterEach(() => {
+      consoleLogSpy.mockReset()
+      wrapper.unmount()
+    })
+
+    it('calls console.log', () => {
+      expect(consoleLogSpy).toHaveBeenCalledTimes(1)
+    })
+
+    it('calls console.log with "registered"', () => {
+      expect(consoleLogSpy).toBeCalledWith('registered')
+    })
+  })
+
+  describe('when the login button is pressed', () => {
+    let wrapper
+    let consoleLogSpy
+
+    beforeEach(() => {
+      consoleLogSpy = jest.spyOn(console, 'log')
+      wrapper = mount(<Welcome />)
+      wrapper.find('.Welcome__login-button').at(1).simulate('click')
+    })
+
+    afterEach(() => {
+      consoleLogSpy.mockReset()
+      wrapper.unmount()
+    })
+
+    it('calls console.log', () => {
+      expect(consoleLogSpy).toHaveBeenCalledTimes(1)
+    })
+
+    it('calls console.log with "logged in"', () => {
+      expect(consoleLogSpy).toBeCalledWith('logged in')
+    })
   })
 })
