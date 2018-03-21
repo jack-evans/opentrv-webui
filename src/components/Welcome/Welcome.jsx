0005677c84fe141f7b0f77d09736e0a96ae85f66
Jack Evans <j.evans.1310@gmail.com>
2018-03-21 21:55:28 +0000

    continue to build welcome page

--- src/components/Welcome/Welcome.jsx
+++ src/components/Welcome/Welcome.jsx
@@ -6,17 +6,20 @@ class Welcome extends Component {
     return (
       <div className='Welcome'>
         <img src={require('./opentrv.jpg')} alt='OpenTRV logo' />
+        <h2>Welcome to OpenTRV</h2>
+        <h3>New User? Click Register to create an account</h3>
+        <h3>Existing User? Login to view your TRV system</h3>
         <div className='Welcome__buttons'>
           <Button
             kind='secondary'
             className='Welcome__register-button'
-            onClick={() => (console.log('registered'))}
+            onClick={() => window.location.assign('/register')}
           >
             Register
           </Button>
           <Button
             className='Welcome__login-button'
-            onClick={() => (console.log('logged in'))}
+            onClick={() => window.location.assign('/login')}
           >
             Login
           </Button>
--- src/components/Welcome/Welcome.spec.jsx
+++ src/components/Welcome/Welcome.spec.jsx
@@ -12,49 +12,47 @@ describe('Welcome.jsx', () => {
 
   describe('when the register button is pressed', () => {
     let wrapper
-    let consoleLogSpy
 
     beforeEach(() => {
-      consoleLogSpy = jest.spyOn(console, 'log')
+      global.location.assign = jest.fn()
       wrapper = mount(<Welcome />)
       wrapper.find('.Welcome__register-button').at(1).simulate('click')
     })
 
     afterEach(() => {
-      consoleLogSpy.mockReset()
+      jest.resetAllMocks()
       wrapper.unmount()
     })
 
-    it('calls console.log', () => {
-      expect(consoleLogSpy).toHaveBeenCalledTimes(1)
+    it('calls global.location.assign', () => {
+      expect(global.location.assign).toHaveBeenCalledTimes(1)
     })
 
-    it('calls console.log with "registered"', () => {
-      expect(consoleLogSpy).toBeCalledWith('registered')
+    it('calls global.location.assign with "/register"', () => {
+      expect(global.location.assign).toBeCalledWith('/register')
     })
   })
 
   describe('when the login button is pressed', () => {
     let wrapper
-    let consoleLogSpy
 
     beforeEach(() => {
-      consoleLogSpy = jest.spyOn(console, 'log')
+      global.location.assign = jest.fn()
       wrapper = mount(<Welcome />)
       wrapper.find('.Welcome__login-button').at(1).simulate('click')
     })
 
     afterEach(() => {
-      consoleLogSpy.mockReset()
+      jest.resetAllMocks()
       wrapper.unmount()
     })
 
-    it('calls console.log', () => {
-      expect(consoleLogSpy).toHaveBeenCalledTimes(1)
+    it('calls global.location.assign', () => {
+      expect(global.location.assign).toHaveBeenCalledTimes(1)
     })
 
-    it('calls console.log with "logged in"', () => {
-      expect(consoleLogSpy).toBeCalledWith('logged in')
+    it('calls global.location.assign with "/login"', () => {
+      expect(global.location.assign).toBeCalledWith('/login')
     })
   })
 })
