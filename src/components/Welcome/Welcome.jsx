fe264e1ce7af8974fe28ae561525be934eef58bf
Jack Evans <j.evans.1310@gmail.com>
2018-03-22 12:05:04 +0000

    add styling to the welcome page component

--- src/components/Welcome/Welcome.jsx
+++ src/components/Welcome/Welcome.jsx
@@ -5,24 +5,28 @@ class Welcome extends Component {
   render () {
     return (
       <div className='Welcome'>
-        <img src={require('./opentrv.jpg')} alt='OpenTRV logo' />
-        <h2>Welcome to OpenTRV</h2>
-        <h3>New User? Click Register to create an account</h3>
-        <h3>Existing User? Login to view your TRV system</h3>
-        <div className='Welcome__buttons'>
-          <Button
-            kind='secondary'
-            className='Welcome__register-button'
-            onClick={() => window.location.assign('/register')}
-          >
-            Register
-          </Button>
-          <Button
-            className='Welcome__login-button'
-            onClick={() => window.location.assign('/login')}
-          >
-            Login
-          </Button>
+        <div>
+          <img style={{paddingBottom: '50px'}} src={require('./opentrv.jpg')} alt='OpenTRV logo' />
+          <h2 style={{paddingTop: '10px', paddingBottom: '10px'}}>Welcome to OpenTRV</h2>
+          <h3 style={{paddingBottom: '10px'}}>New User? Click Register to create an account</h3>
+          <h3 style={{paddingBottom: '10px'}}>Existing User? Login to view your TRV system</h3>
+          <div className='Welcome__buttons'>
+            <Button
+              style={{width: '120px'}}
+              kind='secondary'
+              className='Welcome__register-button'
+              onClick={() => window.location.assign('/register')}
+            >
+              Register
+            </Button>
+            <Button
+              style={{width: '120px'}}
+              className='Welcome__login-button'
+              onClick={() => window.location.assign('/login')}
+            >
+              Login
+            </Button>
+          </div>
         </div>
       </div>
     )
