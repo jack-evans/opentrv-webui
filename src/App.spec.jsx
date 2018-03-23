6dcbc6203859ef6722560de95d857051057c9405
Jack Evans <j.evans.1310@gmail.com>
2018-03-23 14:35:57 +0000

    fix linting

--- src/App.spec.jsx
+++ src/App.spec.jsx
@@ -1,9 +1,4 @@
-import React from 'react'
 import App from './App'
-import Enzyme, { shallow } from 'enzyme'
-import Adapter from 'enzyme-adapter-react-16'
-
-Enzyme.configure({ adapter: new Adapter() })
 
 describe('App.jsx', () => {
   'use strict'
