e1b7a917cf9ba8b54f7c955c40c5f317cff7f247
Jack Evans <j.evans.1310@gmail.com>
2018-03-24 12:52:52 +0000

    change welcome component to be function rather than class

--- src/components/Welcome/Welcome.jsx
+++ src/components/Welcome/Welcome.jsx
@@ -1,36 +1,32 @@
-import React, { Component } from 'react'
+import React from 'react'
 import { Button } from 'carbon-components-react'
 
-class Welcome extends Component {
-  render () {
-    return (
-      <div className='Welcome'>
-        <div>
-          <img style={{paddingBottom: '50px'}} src={require('./opentrv.jpg')} alt='OpenTRV logo' />
-          <h2 style={{paddingTop: '10px', paddingBottom: '10px'}}>Welcome to OpenTRV</h2>
-          <h3 style={{paddingBottom: '10px'}}>New User? Click Register to create an account</h3>
-          <h3 style={{paddingBottom: '10px'}}>Existing User? Login to view your TRV system</h3>
-          <div className='Welcome__buttons'>
-            <Button
-              style={{width: '120px'}}
-              kind='secondary'
-              className='Welcome__register-button'
-              onClick={() => window.location.assign('/register')}
-            >
-              Register
-            </Button>
-            <Button
-              style={{width: '120px'}}
-              className='Welcome__login-button'
-              onClick={() => window.location.assign('/login')}
-            >
-              Login
-            </Button>
-          </div>
-        </div>
+const Welcome = () => (
+  <div className='Welcome'>
+    <div>
+      <img style={{paddingBottom: '100px', paddingTop: '100px'}} src={require('./opentrv.jpg')} alt='OpenTRV logo' />
+      <h2 style={{paddingTop: '10px', paddingBottom: '10px'}}>Welcome to OpenTRV</h2>
+      <p style={{paddingBottom: '10px'}}>New User? Click Register to create an account</p>
+      <p style={{paddingBottom: '10px'}}>Existing User? Login to view your TRV system</p>
+      <div className='Welcome__buttons'>
+        <Button
+          style={{width: '120px'}}
+          kind='secondary'
+          className='Welcome__register-button'
+          onClick={() => window.location.assign('/register')}
+        >
+          Register
+        </Button>
+        <Button
+          style={{width: '120px'}}
+          className='Welcome__login-button'
+          onClick={() => window.location.assign('/login')}
+        >
+          Login
+        </Button>
       </div>
-    )
-  }
-}
+    </div>
+  </div>
+)
 
 export default Welcome
