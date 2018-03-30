6ffb8908c2731586de733581bad7df13620b63d3
Jack Evans <j.evans.1310@gmail.com>
2018-03-30 12:39:35 +0100

    fully cover with unit tests

--- src/components/PrivateRoute/PrivateRoute.spec.jsx
+++ src/components/PrivateRoute/PrivateRoute.spec.jsx
@@ -1,7 +1,10 @@
 import React from 'react'
 import PrivateRoute from './PrivateRoute'
-import Enzyme, { shallow } from 'enzyme'
+import { MemoryRouter } from 'react-router-dom'
+import Enzyme, { shallow, mount } from 'enzyme'
 import Adapter from 'enzyme-adapter-react-16'
+import { isAuthenticated } from '../../utils/auth'
+jest.mock('../../utils/auth')
 
 Enzyme.configure({ adapter: new Adapter() })
 
@@ -14,5 +17,38 @@ describe('PrivateRoute.jsx', () => {
 
   it('renders without crashing', () => {
     expect(shallow(<PrivateRoute path='/test' component={TestComponenent} />))
+  })
+
+  describe('when isAuthenticated returns true', () => {
+    beforeEach(() => {
+      isAuthenticated.mockReturnValue(true)
+    })
+
+    it('returns the component', () => {
+      const wrapper = mount(
+        <MemoryRouter>
+          <PrivateRoute path='/test' component={TestComponenent} />
+        </MemoryRouter>
+      )
+      let component = wrapper.find('Route').props().render()
+      component = component.type()
+      expect(component.props.children).toEqual('test')
+    })
+  })
+
+  describe('when isAuthenticated returns false', () => {
+    beforeEach(() => {
+      isAuthenticated.mockReturnValue(false)
+    })
+
+    it('redirects to "/"', () => {
+      const wrapper = mount(
+        <MemoryRouter>
+          <PrivateRoute path='/test' component={TestComponenent} />
+        </MemoryRouter>
+      )
+      let component = wrapper.find('Route').props().render()
+      expect(component.props.to).toEqual('/')
+    })
   })
 })
